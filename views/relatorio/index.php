3eb60d2d1a744606664dacfdf49688a78093d049
André Walker <andre.walkermo@gmail.com>
2020-04-03 19:49:16 -0300

    add https ao script

--- views/relatorio/index.php
+++ views/relatorio/index.php
@@ -72,10 +72,10 @@
 
 </main>
 
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.js"></script>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.14/jquery-ui.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.14/jquery-ui.min.js"></script>
 <script type="text/javascript" src="../../assets/js/datepicker-pt-BR.js"></script>
-<link rel="stylesheet" type="text/css" media="screen" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.14/themes/base/jquery-ui.css">
+<link rel="stylesheet" type="text/css" media="screen" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.14/themes/base/jquery-ui.css">
 <script type="text/javascript">
     // $( ".week-picker" ).hide()
 
