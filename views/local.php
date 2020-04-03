1ce7e3f99caabdf7c3edb512ccd76313933a059c
André Walker <andre.walkermo@gmail.com>
2020-04-03 04:37:02 -0300

    muda local pasta .htpasswd

---
+++ views/local.php
@@ -0,0 +1,5 @@
+<?php
+$dir = dirname(__FILE__);
+
+echo "<p>Caminho absoluto da sua pasta: " . $dir . "</p>";
+echo "<p>Caminho absoluto da sua pasta aonde estara o seu .htpasswd: " . $dir . "/.htpasswd" . "</p>";
